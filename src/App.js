872993ebfc71a848e9fde4e7f449ef6792ac2056
zhouyuhe <yuhe.zhou@weareadaptive.com>
2019-12-04 14:06:41 +0000

    add comment on it

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useReducer, useEffect } from 'react';
 import './App.css';
-
+//format in correct order
 const formatWithZero = (unit) => unit < 10 ? '0' + unit : String(unit)
 const formatTime = (timeInMiliSeconds) => {
   const mins = Math.floor(timeInMiliSeconds / 6000)
@@ -8,21 +8,24 @@ const formatTime = (timeInMiliSeconds) => {
   const milisec = Math.floor(((timeInMiliSeconds) % 100))
   return `${formatWithZero(mins)}:${formatWithZero(sec)}.${formatWithZero(milisec)}`
 }
-
+// setup the initial state
 const initialState = { isRunning: false, timeElapsed: 0, previousTime: 0, lapTime: [] }
+//make them constant -- consistent
 const statStop = 'statStop'
 const lapReset = 'lapReset'
 const incrementTimer = 'incrementTimer'
-
+//find the max and min laps
 const getMinLap = (laps) => Math.min.apply(Math, laps)
 const getMaxLap = (laps) => Math.max.apply(Math, laps)
 
 const reducer = (state, action) => {
   switch (action) {
+    //changing betweern start and stop when click the button
     case statStop:
       return { ...state, isRunning: !state.isRunning }
     case incrementTimer:
       return { ...state, timeElapsed: state.timeElapsed + 1 }
+      //lap condition
     case lapReset:
       if (!state.isRunning) {
         return { isRunning: false, timeElapsed: 0, previousTime: 0, lapTime: [] }
@@ -30,16 +33,15 @@ const reducer = (state, action) => {
         return {
           ...state,
           previousTime: state.timeElapsed,
+          // calculate the correct time
           lapTime: [state.timeElapsed - state.previousTime, ...state.lapTime]
         }
       }
+      // switch condition always do default one !!!
     default:
       throw new Error()
   }
 }
-
-// must be outside app component, otherwise it loop again
-let interval
 
 const App = () => {
 
@@ -47,17 +49,17 @@ const App = () => {
 
   useEffect(() => {
     if (state.isRunning) {
-      interval = setInterval(() =>
+      const interval = setInterval(() =>
         dispatch(incrementTimer)
         , 10)
-    } else {
-      clearInterval(interval)
+        return () => clearInterval(interval)
     }
-    return () => clearInterval()
   }, [state.isRunning])
 
+  // destructure the state
   const { isRunning, timeElapsed, lapTime } = state
   const startStopLblChange = isRunning ? 'Stop' : 'Start'
+  //match the condition for lap
   const lapRestetLblChange = isRunning || timeElapsed === 0 ? 'Lap' : 'Reset'
 
   const min = getMinLap(lapTime)
